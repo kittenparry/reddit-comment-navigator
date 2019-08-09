116877692ca0128672ed3f59b87273c67e5bde5f
Edvin Boul <kittenparry@users.noreply.github.com>
2019-08-09 22:56:25 +0300

    Focus change only if out of view (external func)
    Chris Ferdinandi's function under MIT license

--- rcn.user.js
+++ rcn.user.js
@@ -96,7 +96,7 @@ change_sel_com_same = (dire) => {
 		rcn_sel_el = rcn_sel_el.parentElement.parentElement.children[rcn_sc].children[2];
 	}
 	rcn_sel_el.classList.add(rcn_sc_sel);
-	rcn_sel_el.scrollIntoView();
+	check_if_in_view();
 };
 
 change_sel_com_lvl = (dire) => {
@@ -114,8 +114,48 @@ change_sel_com_lvl = (dire) => {
 		rcn_dep -= 1;
 	}
 	rcn_sel_el.classList.add(rcn_sc_sel);
-	rcn_sel_el.scrollIntoView();
+	check_if_in_view();
 }
+
+/* * * * * * * * *
+ * EXTERNAL BEGIN *
+ * * * * * * * * */
+
+/*!
+ * Source: https://gomakethings.com/how-to-check-if-any-part-of-an-element-is-out-of-the-viewport-with-vanilla-js/
+ * Check if an element is out of the viewport
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}  elem The element to check
+ * @return {Object}     A set of booleans for each side of the element
+ */
+rcn_is_out_of_viewport = (elem) => {
+
+	// Get element's bounding
+	var bounding = elem.getBoundingClientRect();
+
+	// Check if it's out of the viewport on each side
+	var out = {};
+	out.top = bounding.top < 0;
+	out.left = bounding.left < 0;
+	out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
+	out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+	out.any = out.top || out.left || out.bottom || out.right;
+	out.all = out.top && out.left && out.bottom && out.right;
+
+	return out;
+};
+
+/* * * * * * * *
+ * EXTERNAL END *
+ * * * * * * * */
+
+check_if_in_view = () => {
+	let is_out = rcn_is_out_of_viewport(rcn_sel_el);
+	if (is_out.any) {
+		rcn_sel_el.scrollIntoView();
+	}
+};
+
 
 // key functions
 red_com_nav = (e) => {
